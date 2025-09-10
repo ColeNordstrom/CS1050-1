3976d098ac5e20448bf674bea1311aaaa1bac3e8
ColeNordstrom <colenordstrom97@gmail.com>
2025-09-10 14:05:03 -0600

    second inital commit (cleaning things up)

---
+++ Eclipse Workspace/someProject/someProject/Classsss.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package someProject;
+
+/**
+ * 
+ */
+public class Classsss {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
